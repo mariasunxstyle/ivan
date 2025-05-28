ef07398b6a21cdd1ea89f4f1a7b89f97f59fc30d
mariasunxstyle <tohmian@mail.ru>
2025-05-28 16:18:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import asyncio
 from aiogram import executor
-from handlers import dp
+from bot import dp
+import handlers  # noqa
 
 if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
     executor.start_polling(dp, skip_updates=True)
