c6f44d028b5bbe8d5efc2c076643b3b8f79ba65d
mariasunxstyle <tohmian@mail.ru>
2025-05-28 16:49:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-<PASTE_FULL_main.py_CONTENT_HERE>
+import asyncio
+import logging
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.utils import executor
+from handlers import register_handlers
+from dotenv import load_dotenv
+
+load_dotenv()
+API_TOKEN = os.getenv("TOKEN")
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+register_handlers(dp, bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp, skip_updates=True)
