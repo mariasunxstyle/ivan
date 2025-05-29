2ae0285ebefa56c5d325760c02cc8a6c165394bd
mariasunxstyle <tohmian@mail.ru>
2025-05-29 19:18:58 +0300

    Delete menu_keyboard.py

--- menu_keyboard.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-menu_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-menu_keyboard.add(
-    KeyboardButton("▶️ Начать"),
-    KeyboardButton("ℹ️ Инфо"),
-    KeyboardButton("📋 Выбрать шаг")
-)
