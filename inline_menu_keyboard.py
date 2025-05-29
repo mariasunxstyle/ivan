7f245e968ae63f9e2717a209ffc5fe08eb3f6b19
mariasunxstyle <tohmian@mail.ru>
2025-05-29 19:17:52 +0300

    Delete inline_menu_keyboard.py

--- inline_menu_keyboard.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-inline_menu = InlineKeyboardMarkup(row_width=1)
-inline_menu.add(
-    InlineKeyboardButton("▶️ Начать", callback_data="start"),
-    InlineKeyboardButton("ℹ️ Инфо", callback_data="info"),
-    InlineKeyboardButton("📋 Выбрать шаг", callback_data="choose_step")
-)
