1ec8c1eaa8df40e0f57f5847b83bea93296ecdfa
mariasunxstyle <tohmian@mail.ru>
2025-05-30 11:30:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import asyncio
+
 import logging
 import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from dotenv import load_dotenv
 
-from keyboards import steps_keyboard, get_control_keyboard, get_continue_keyboard, control_keyboard_full, end_keyboard
 from state import user_state, tasks, step_completion_shown
+from keyboards import steps_keyboard, get_continue_keyboard, get_control_keyboard, control_keyboard_full, end_keyboard
 from texts import GREETING, INFO_TEXT
 from timer import start_position
 
@@ -15,14 +15,17 @@ API_TOKEN = os.getenv("TOKEN")
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands=['start'])
 async def send_welcome(msg: types.Message):
     await msg.answer(GREETING, reply_markup=steps_keyboard)
+
 
 @dp.message_handler(commands=['info'])
 @dp.message_handler(lambda m: m.text == "ℹ️ Инфо")
 async def info(msg: types.Message):
     await msg.answer(INFO_TEXT)
+
 
 @dp.message_handler(lambda m: m.text.startswith("Шаг "))
 async def handle_step(msg: types.Message):
@@ -31,12 +34,14 @@ async def handle_step(msg: types.Message):
     step_completion_shown.discard(msg.chat.id)
     await start_position(bot, msg.chat.id)
 
+
 @dp.message_handler(lambda m: m.text == "⏭️ Пропустить")
 async def skip(msg: types.Message):
     uid = msg.chat.id
     t = tasks.pop(uid, None)
     if t: t.cancel()
     await start_position(bot, uid)
+
 
 @dp.message_handler(lambda m: m.text == "⛔ Завершить")
 async def end(msg: types.Message):
@@ -46,6 +51,7 @@ async def end(msg: types.Message):
     user_state[uid] = {"last_step": user_state.get(uid, {}).get("step", 1)}
     step_completion_shown.discard(uid)
     await bot.send_message(uid, "Сеанс завершён. Можешь вернуться позже и начать заново ☀️", reply_markup=end_keyboard)
+
 
 @dp.message_handler(lambda m: m.text.startswith("↩️"))
 async def back(msg: types.Message):
@@ -62,6 +68,7 @@ async def back(msg: types.Message):
     await bot.send_message(uid, f"Шаг {user_state[uid]['step']}")
     await start_position(bot, uid)
 
+
 @dp.message_handler(lambda m: m.text == "📋 Вернуться к шагам")
 async def menu(msg: types.Message):
     uid = msg.chat.id
@@ -70,6 +77,7 @@ async def menu(msg: types.Message):
     user_state.pop(uid, None)
     step_completion_shown.discard(uid)
     await msg.answer("Выбери шаг:", reply_markup=steps_keyboard)
+
 
 @dp.message_handler(lambda m: m.text == "▶️ Продолжить")
 async def continue_step(msg: types.Message):
@@ -82,6 +90,7 @@ async def continue_step(msg: types.Message):
     step_completion_shown.discard(uid)
     await bot.send_message(uid, f"Шаг {state['step']}")
     await start_position(bot, uid)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
