4faaa328e12752d039e9a7e28bd05f30fa678147
mariasunxstyle <tohmian@mail.ru>
2025-05-31 12:53:44 +0300

    Add files via upload

--- timer.py
+++ timer.py
@@ -1,6 +1,1 @@
-import asyncio
-from aiogram import types
-
-async def run_timer(message: types.Message, position: str, minutes: float):
-    await message.answer(f"{position} — {int(minutes)} мин\n⏳ Таймер запущен...")
-    await asyncio.sleep(int(minutes * 60))
+# timer.py — логика таймера и состояний
